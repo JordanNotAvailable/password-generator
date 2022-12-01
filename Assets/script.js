d0db9e5c79684fb5c4119dc2d13cd54a81fe8b7d
Jordan O'Grady <jord_ogrady@hotmail.com>
2022-12-01 23:29:36 +1100

    fixed issue with generated password not showing

--- Assets/script.js
+++ Assets/script.js
@@ -2,14 +2,10 @@
 var generateBtn = document.querySelector("#generate");
 var symbols = [ '@', '%', '+', '\\', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', 
 '[', '~', '-', '_', '.' ];
-var numebrs = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-var lowerCharacters = [
-  'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 
-  'x', 'y', 'z' ];
-var upperCharacters = [
-  'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 
-  'X', 'Y', 'Z' ];
-
+var numbers = ['0123456789'];
+var lowerCharacters = [ 'abcdefghijklmnopqrstuvwxyz' ];
+var upperCharacters = [ 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' ];
+var count = 0
 // Write password to the #password input
 
   // 1. prompt the used for password criteria
@@ -19,19 +15,19 @@ function lengthConfirmation () {
   lengthConfirmation = prompt("How many charecters would you like, between 8 and 128 😃 ", "Answer here!");
 }
 
+// new empty array to join previous arrays to
+var abc = []
 
 function generatePassword() {
   console.log("button clicked");
   lengthConfirmation();
-  var userChoice = window.prompt
 
-  if (lengthConfirmation < 8 || lengthConfirmation > 129) {
+  if (lengthConfirmation < 8 || lengthConfirmation > 128) {
     alert('Thats not a valid answer, try again!');
     lengthConfirmation();
-
   }
 
-  //    b. uppercase, lowercase, numbers or spacial characters
+//    b. uppercase, lowercase, numbers or spacial characters
 
 var lowerCaseConfirmation =  confirm ("Would you like lowercase letters ? 😃 ", "Answer here!");
 
@@ -39,32 +35,57 @@ var upperCaseConfirmation = confirm("Would you like UPPERCASE letters ? 😃 ", 
 
 var numberConfirmation = confirm("Would you like numbers ? 😃 ", "Answer here!");
 
-var sybolConfirmation =  confirm("Would you like special characters ? 😃 ", "Answer here!");
+var symbolConfirmation =  confirm("Would you like special characters ? 😃 ", "Answer here!");
 
-if (lowerCaseConfirmation === false && upperCaseConfirmation === false && numberConfirmation === false && sybolConfirmation === false) {
+if (lowerCaseConfirmation === false && upperCaseConfirmation === false && numberConfirmation === false && symbolConfirmation === false) {
   alert('Thats not a valid answer, try again!');
 }
 
+var generatedPassword = '';
+
+// 2. validate the input
 
 
- 
-//  if (!userChoice) {
-//   return(prompt("Would you like UPPERCASE letters ? 😃 ", "Answer here!"));
-// }
+if (lowerCaseConfirmation == true) {
+  // 
+  abc.push(lowerCharacters) 
+  console.log(lowerCharacters)
+}
 
-  // 2. validate the input
-  // 3. generate the password
-  
-// 4. display the passwod on the page
-  return "password var goes here"
+if (upperCaseConfirmation == true) {
+  // combined upper case into joined array
+  abc.push(upperCharacters) 
+}
+
+if (numberConfirmation == true) {
+  //combined numbers into joined array
+  abc.push(numbers) 
+}
+
+if (symbolConfirmation == true) {
+  // combine symbols into joined array
+  abc.push(symbols) 
+}
+
+// making the abc array into a string
+abc = abc.join("")
+console.log(abc)
+
+// generate the password from above criteria
+for (var i=0; i < lengthConfirmation; i++) {
+  generatedPassword += abc.charAt(Math.floor(Math.random() * abc.length));
+  console.log(generatedPassword);
+}
+
+//display the passwod on the page
+console.log(generatedPassword);
+  return generatedPassword;
 }
 
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
